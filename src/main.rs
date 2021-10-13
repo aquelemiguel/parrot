3830ae7fba5968d5ac129a36c247ad0a9e21981b
VenturaPereira <venturasousapereira@hotmail.com>
2021-10-13 09:02:30 +0100

    Serven deaf on the bot

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use serenity::{Client, async_trait, client::{Context, EventHandler}, framework::{standard::macros::group, StandardFramework}, model::{gateway::Ready, prelude::Activity}};
+use serenity::{Client, async_trait, builder::{EditMember}, client::{Context, EventHandler}, framework::{standard::macros::group, StandardFramework}, model::{gateway::Ready, guild::{GuildStatus, Member}, id::GuildId, prelude::{Activity, VoiceState}}};
 use songbird::SerenityInit;
 use std::env;
 
@@ -31,7 +31,22 @@ impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
         ctx.set_activity(Activity::listening("!play")).await;
+      
+    
     }
+            
+   async fn voice_state_update(&self, ctx: Context, guild: Option<GuildId>, _old: Option<VoiceState>, new: VoiceState) {
+       if new.user_id == ctx.http.get_current_user().await.unwrap().id && !new.deaf {
+            guild.unwrap().edit_member(&ctx.http, new.user_id, |n| n.deafen(true)).await.unwrap();
+       }
+    }
+
+   
+
+  
+
+
+
 }
 
 #[tokio::main]
@@ -39,10 +54,13 @@ async fn main() {
     dotenv::dotenv().expect("Failed to load .env file");
 
     let framework = StandardFramework::new()
-        .configure(|c| c.prefix("!"))
-        .group(&GENERAL_GROUP);
+        .configure(|c| c.prefix("-"))
+        .group(&GENERAL_GROUP)
+        ;
 
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    
 
     let mut client = Client::builder(token)
         .event_handler(Handler)
@@ -51,6 +69,8 @@ async fn main() {
         .await
         .expect("Error creating client");
 
+    
+    
     // Start listening for events by starting a single shard
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
