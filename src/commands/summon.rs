7cc17f3436c1f9e4fa673d13eccf544dfb625c44
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-30 23:57:51 +0000

    refactor: summon

--- src/commands/summon.rs
+++ src/commands/summon.rs
@@ -11,42 +11,39 @@ use crate::utils::send_simple_message;
 #[command]
 pub async fn summon(ctx: &Context, msg: &Message) -> CommandResult {
     let guild = msg.guild(&ctx.cache).await.unwrap();
+    let manager = songbird::get(ctx).await.unwrap();
 
-    // Find the voice channel where the author is at
     let channel_opt = guild
         .voice_states
         .get(&msg.author.id)
         .and_then(|voice_state| voice_state.channel_id);
 
-    if let Some(channel_id) = channel_opt {
-        let manager = songbird::get(ctx).await.unwrap();
+    let channel_id = match channel_opt {
+        Some(channel_id) => channel_id,
+        None => return send_simple_message(&ctx.http, msg, AUTHOR_NOT_FOUND).await 
+    };
 
-        if let Some(call) = manager.get(guild.id) {
-            let handler = call.lock().await;
-            let has_current_connection = handler.current_connection().is_some();
-            drop(handler);
+    if let Some(call) = manager.get(guild.id) {
+        let handler = call.lock().await;
+        let has_current_connection = handler.current_connection().is_some();
+        drop(handler);
 
-            // Bot is already in the channel
-            if has_current_connection {
-                return Ok(());
-            }
-
-            // Bot might have been disconnected manually
-            manager.remove(guild.id).await.unwrap();
+        // bot is already in the channel
+        if has_current_connection {
+            return Ok(());
         }
 
-        // Now that we've ensured the bot isn't connected, join the channel
-        manager.join(guild.id, channel_id).await.1?;
-
-        send_simple_message(
-            &ctx.http,
-            msg,
-            &format!("Joining **{}**!", channel_id.mention()),
-        )
-        .await;
-    } else {
-        send_simple_message(&ctx.http, msg, AUTHOR_NOT_FOUND).await;
+        // bot might have been disconnected manually
+        manager.remove(guild.id).await.unwrap();
     }
 
-    Ok(())
+    // join the channel
+    manager.join(guild.id, channel_id).await.1?;
+
+    return send_simple_message(
+        &ctx.http,
+        msg,
+        &format!("Joining **{}**!", channel_id.mention()),
+    )
+    .await;
 }
