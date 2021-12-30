d6ad8644d7e104e519b795c37e28c73a5a68b695
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-30 23:18:57 +0000

    refactor: resume command

--- src/commands/resume.rs
+++ src/commands/resume.rs
@@ -12,23 +12,22 @@ use crate::{
 #[command]
 async fn resume(ctx: &Context, msg: &Message) -> CommandResult {
     let guild_id = msg.guild(&ctx.cache).await.unwrap().id;
-    let manager = songbird::get(ctx)
-        .await
-        .expect("Could not retrieve Songbird voice client");
+    let manager = songbird::get(ctx).await.unwrap();
 
-    if let Some(call) = manager.get(guild_id) {
-        let handler = call.lock().await;
-        let queue = handler.queue();
+    let call = match manager.get(guild_id) {
+        Some(call) => call,
+        None => return send_simple_message(&ctx.http, msg, NO_VOICE_CONNECTION).await,
+    };
 
-        if queue.is_empty() {
-            drop(handler);
-            send_simple_message(&ctx.http, msg, QUEUE_IS_EMPTY).await;
-        } else if queue.resume().is_ok() {
-            drop(handler);
-            send_simple_message(&ctx.http, msg, "Resumed!").await;
-        }
-    } else {
-        send_simple_message(&ctx.http, msg, NO_VOICE_CONNECTION).await;
+    let handler = call.lock().await;
+    let queue = handler.queue();
+
+    if queue.is_empty() {
+        return send_simple_message(&ctx.http, msg, QUEUE_IS_EMPTY).await;
+    }
+
+    if queue.resume().is_ok() {
+        return send_simple_message(&ctx.http, msg, "Resumed!").await;
     }
 
     Ok(())
