bb90468de9b15e4701b5a518875e62113ac24409
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-30 23:51:46 +0000

    refactor: seek

--- src/commands/seek.rs
+++ src/commands/seek.rs
@@ -16,40 +16,35 @@ async fn seek(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     let guild_id = msg.guild(&ctx.cache).await.unwrap().id;
     let manager = songbird::get(ctx).await.unwrap();
 
-    if let Some(call) = manager.get(guild_id) {
-        let seek_time = match args.single::<String>() {
-            Ok(t) => t,
-            Err(_) => {
-                send_simple_message(&ctx.http, msg, MISSING_TIMESTAMP).await;
-                return Ok(());
-            }
-        };
+    let call = match manager.get(guild_id) {
+        Some(call) => call,
+        None => return send_simple_message(&ctx.http, msg, NO_VOICE_CONNECTION).await 
+    };
 
-        let mut timestamp = Args::new(&seek_time, &[Delimiter::Single(':')]);
-        let (minutes, seconds) = (timestamp.single::<u64>(), timestamp.single::<u64>());
+    let seek_time = match args.single::<String>() {
+        Ok(t) => t,
+        Err(_) => return send_simple_message(&ctx.http, msg, MISSING_TIMESTAMP).await
+    };
 
-        if minutes.as_ref().and(seconds.as_ref()).is_err() {
-            send_simple_message(&ctx.http, msg, TIMESTAMP_PARSING_FAILED).await;
-            return Ok(());
-        }
+    let mut timestamp = Args::new(&seek_time, &[Delimiter::Single(':')]);
+    let (minutes, seconds) = (timestamp.single::<u64>(), timestamp.single::<u64>());
 
-        let timestamp = minutes.unwrap() * 60 + seconds.unwrap();
-
-        let handler = call.lock().await;
-        let track = handler.queue().current().unwrap();
-        drop(handler);
-
-        track.seek_time(Duration::from_secs(timestamp)).unwrap();
-
-        send_simple_message(
-            &ctx.http,
-            msg,
-            &format!("Seeked current track to **{}**!", seek_time),
-        )
-        .await;
-    } else {
-        send_simple_message(&ctx.http, msg, NO_VOICE_CONNECTION).await;
+    if minutes.as_ref().and(seconds.as_ref()).is_err() {
+        return send_simple_message(&ctx.http, msg, TIMESTAMP_PARSING_FAILED).await;
     }
 
-    Ok(())
+    let timestamp = minutes.unwrap() * 60 + seconds.unwrap();
+
+    let handler = call.lock().await;
+    let track = handler.queue().current().unwrap();
+    drop(handler);
+
+    track.seek_time(Duration::from_secs(timestamp)).unwrap();
+
+    return send_simple_message(
+        &ctx.http,
+        msg,
+        &format!("Seeked current track to **{}**!", seek_time),
+    )
+    .await;
 }
