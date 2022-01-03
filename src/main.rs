c13e455d93192d3822461a85e62e0ceef9399370
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-01-03 20:30:40 +0000

    version: 0.2.0 (#66)

--- src/main.rs
+++ src/main.rs
@@ -84,7 +84,6 @@ async fn main() {
         .await
         .expect("Error creating client");
 
-    // Start listening for events by starting a single shard
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
     }
