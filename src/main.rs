35d927a09cf2e55b7f6254f7b195ba04bd8bf895
Afonso Ramos <afonsojorgeramos@gmail.com>
2022-01-06 19:58:56 +0000

    add default prefix env var

--- src/main.rs
+++ src/main.rs
@@ -80,15 +80,15 @@ async fn main() {
             c.dynamic_prefix(|ctx, msg| {
                 Box::pin(async move {
                     let prefixes = get_prefixes();
-
+                    let default_prefix = env::var("PREFIX").unwrap_or_else(|_| "!".to_string());
                     let guild_id = msg.guild(&ctx.cache).await.unwrap().id;
 
-                    if let Some(serde_json::Value::String(prefix)) =
+                    if let Some(serde_json::Value::String(guild_prefix)) =
                         prefixes.get(guild_id.0.to_string())
                     {
-                        Some(prefix.clone())
+                        Some(guild_prefix.clone())
                     } else {
-                        Some("!".to_string())
+                        Some(default_prefix)
                     }
                 })
             })
