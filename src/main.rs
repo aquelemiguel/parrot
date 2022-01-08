984db90673933ef16d8152c5a7ec14ab2999fbe9
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-01-08 20:14:49 +0000

    refactor: simplify main and move some logic to lib for external use

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,14 @@
-use serenity::{
-    framework::{standard::macros::group, StandardFramework},
-    Client,
-};
-use songbird::SerenityInit;
-use std::env;
-
-use parrot::{
-    commands::{
-        clear::*,
-        genius::{explain::*, lyrics::*},
-        leave::*,
-        now_playing::*,
-        pause::*,
-        play::*,
-        playtop::*,
-        prefix::*,
-        queue::*,
-        remove::*,
-        repeat::*,
-        resume::*,
-        seek::*,
-        shuffle::*,
-        skip::*,
-        stop::*,
-        summon::*,
-        version::*,
-    },
-    events::serenity_handler::SerenityHandler,
-    strings::DEFAULT_PREFIX,
-    utils::get_prefixes,
-};
-
-#[group]
-#[commands(
-    clear,
-    explain,
-    leave,
-    lyrics,
-    now_playing,
-    pause,
-    play,
-    playtop,
-    prefix,
-    queue,
-    remove,
-    repeat,
-    resume,
-    seek,
-    shuffle,
-    skip,
-    stop,
-    summon,
-    version
-)]
-struct Commands;
+use parrot::client::Client;
+use std::error::Error;
 
 #[tokio::main]
-async fn main() {
-    dotenv::dotenv().ok();
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv::dotenv().unwrap();
 
-    let framework = StandardFramework::new()
-        .configure(|c| {
-            c.dynamic_prefix(|ctx, msg| {
-                Box::pin(async move {
-                    let prefixes = get_prefixes();
-                    let default_prefix =
-                        env::var("PREFIX").unwrap_or_else(|_| DEFAULT_PREFIX.to_string());
-                    let guild_id = msg.guild(&ctx.cache).await.unwrap().id;
+    let mut parrot = Client::new(None).await?;
+    if let Err(why) = parrot.start().await {
+        println!("Fatality! Parrot crashed because: {:?}", why);
+    };
 
-                    if let Some(serde_json::Value::String(guild_prefix)) =
-                        prefixes.get(guild_id.0.to_string())
-                    {
-                        Some(guild_prefix.clone())
-                    } else {
-                        Some(default_prefix)
-                    }
-                })
-            })
-            .prefix("")
-        })
-        .group(&COMMANDS_GROUP);
-
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-
-    let mut client = Client::builder(token)
-        .event_handler(SerenityHandler)
-        .framework(framework)
-        .register_songbird()
-        .await
-        .expect("Error creating client");
-
-    if let Err(why) = client.start().await {
-        println!("An error occurred while running the client: {:?}", why);
-    }
+    Ok(())
 }
