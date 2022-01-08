a360a860ef34998ffb0818a4f8196c07664b68a3
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-01-08 17:00:57 +0000

    fix: show configured prefix by default

--- src/events/serenity_handler.rs
+++ src/events/serenity_handler.rs
@@ -4,9 +4,10 @@ use serenity::{
     model::{
         gateway::Ready,
         id::GuildId,
-        prelude::{OnlineStatus, VoiceState},
+        prelude::{Activity, VoiceState},
     },
 };
+use std::env;
 
 pub struct SerenityHandler;
 
@@ -14,7 +15,10 @@ pub struct SerenityHandler;
 impl EventHandler for SerenityHandler {
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("🦜 {} is connected!", ready.user.name);
-        ctx.set_presence(None, OnlineStatus::Online).await;
+
+        let prefix = env::var("PREFIX").unwrap_or_else(|_| "!".to_string());
+        let activity = Activity::listening(format!("{}play", prefix));
+        ctx.set_activity(activity).await;
     }
 
     async fn voice_state_update(
