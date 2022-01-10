612e570d93d406e4a018e180edc65d85c3ed22ff
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-01-10 01:05:39 +0000

    fix: optional

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    dotenv::dotenv()?;
+    dotenv::dotenv().ok();
 
     let mut parrot = Client::default().await?;
     if let Err(why) = parrot.start().await {
