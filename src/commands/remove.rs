3b491523373109cc9b6cb6da17b83dfb89113236
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-01-21 12:56:23 +0000

    tweak: add negative numbers handling

--- src/commands/remove.rs
+++ src/commands/remove.rs
@@ -22,15 +22,20 @@ pub async fn remove(
 
     let args = interaction.data.options.clone();
 
-    let remove_index = args
+    let signed_remove_index = args
         .first()
         .unwrap()
         .value
         .as_ref()
         .unwrap()
-        .as_u64()
-        .unwrap() as usize;
+        .as_i64()
+        .unwrap();
 
+    if signed_remove_index.is_negative() || signed_remove_index <= 0 {
+        return create_response(&ctx.http, interaction, "Please provide an index >= 1!").await;
+    }
+
+    let remove_index = signed_remove_index as usize;
     let handler = call.lock().await;
     let queue = handler.queue().current_queue();
 
@@ -38,13 +43,6 @@ pub async fn remove(
         create_response(&ctx.http, interaction, QUEUE_IS_EMPTY).await
     } else if queue.len() < remove_index + 1 {
         create_response(&ctx.http, interaction, NO_SONG_ON_INDEX).await
-    } else if remove_index == 0 {
-        create_response(
-            &ctx.http,
-            interaction,
-            "Can't remove currently playing song!",
-        )
-        .await
     } else {
         handler.queue().modify_queue(|v| {
             v.remove(remove_index);
