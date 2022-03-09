d0888525980cd62485aa59d0c1976037c9db4b82
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-03-09 18:11:50 +0000

    feat: verify errors returns the result as well for easy unpack and assignment

--- src/errors.rs
+++ src/errors.rs
@@ -1,11 +1,10 @@
-use serenity::{model::misc::Mention, prelude::SerenityError};
-use std::fmt::Display;
-use std::{error::Error, fmt};
-
 use crate::strings::{
     FAIL_ANOTHER_CHANNEL, FAIL_AUTHOR_DISCONNECTED, FAIL_AUTHOR_NOT_FOUND,
     FAIL_NO_VOICE_CONNECTION, FAIL_WRONG_CHANNEL, NOTHING_IS_PLAYING, QUEUE_IS_EMPTY,
 };
+use serenity::{model::misc::Mention, prelude::SerenityError};
+use std::fmt::{Debug, Display};
+use std::{error::Error, fmt};
 
 #[derive(Debug)]
 pub enum ParrotError {
@@ -21,39 +20,60 @@ pub enum ParrotError {
     Serenity(SerenityError),
 }
 
+/// `ParrotError` implements the `Debug` and `Display` traits
+/// meaning it implements the `Error` trait. This just makes it explicit.
 impl Error for ParrotError {}
 
 impl Display for ParrotError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            ParrotError::Other(msg) => f.write_str(msg),
-            ParrotError::QueueEmpty => f.write_str(QUEUE_IS_EMPTY),
-            ParrotError::NotInRange(param, value, lower, upper) => f.write_str(&format!(
+            Self::Other(msg) => f.write_str(msg),
+            Self::QueueEmpty => f.write_str(QUEUE_IS_EMPTY),
+            Self::NotInRange(param, value, lower, upper) => f.write_str(&format!(
                 "`{param}` should be between {lower} and {upper} but was {value}"
             )),
-            ParrotError::NotConnected => f.write_str(FAIL_NO_VOICE_CONNECTION),
-            ParrotError::AuthorDisconnected(mention) => {
+            Self::NotConnected => f.write_str(FAIL_NO_VOICE_CONNECTION),
+            Self::AuthorDisconnected(mention) => {
                 f.write_fmt(format_args!("{} {}", FAIL_AUTHOR_DISCONNECTED, mention))
             }
-            ParrotError::WrongVoiceChannel => f.write_str(FAIL_WRONG_CHANNEL),
-            ParrotError::AuthorNotFound => f.write_str(FAIL_AUTHOR_NOT_FOUND),
-            ParrotError::AlreadyConnected(mention) => {
+            Self::WrongVoiceChannel => f.write_str(FAIL_WRONG_CHANNEL),
+            Self::AuthorNotFound => f.write_str(FAIL_AUTHOR_NOT_FOUND),
+            Self::AlreadyConnected(mention) => {
                 f.write_fmt(format_args!("{} {}", FAIL_ANOTHER_CHANNEL, mention))
             }
-            ParrotError::NothingPlaying => f.write_str(NOTHING_IS_PLAYING),
-            ParrotError::Serenity(err) => f.write_str(&format!("{err}")),
+            Self::NothingPlaying => f.write_str(NOTHING_IS_PLAYING),
+            Self::Serenity(err) => f.write_str(&format!("{err}")),
+        }
+    }
+}
+
+impl PartialEq for ParrotError {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Other(l0), Self::Other(r0)) => l0 == r0,
+            (Self::NotInRange(l0, l1, l2, l3), Self::NotInRange(r0, r1, r2, r3)) => {
+                l0 == r0 && l1 == r1 && l2 == r2 && l3 == r3
+            }
+            (Self::AuthorDisconnected(l0), Self::AuthorDisconnected(r0)) => {
+                l0.to_string() == r0.to_string()
+            }
+            (Self::AlreadyConnected(l0), Self::AlreadyConnected(r0)) => {
+                l0.to_string() == r0.to_string()
+            }
+            (Self::Serenity(l0), Self::Serenity(r0)) => format!("{l0:?}") == format!("{r0:?}"),
+            _ => core::mem::discriminant(self) == core::mem::discriminant(other),
         }
     }
 }
 
 impl From<SerenityError> for ParrotError {
-    fn from(err: SerenityError) -> ParrotError {
+    fn from(err: SerenityError) -> Self {
         match err {
             SerenityError::NotInRange(param, value, lower, upper) => {
-                ParrotError::NotInRange(param, value as isize, lower as isize, upper as isize)
+                Self::NotInRange(param, value as isize, lower as isize, upper as isize)
             }
-            SerenityError::Other(msg) => ParrotError::Other(msg),
-            _ => ParrotError::Serenity(err),
+            SerenityError::Other(msg) => Self::Other(msg),
+            _ => Self::Serenity(err),
         }
     }
 }
@@ -80,9 +100,37 @@ impl<T, E> ToBool for Result<T, E> {
     }
 }
 
-pub fn verify(condition: impl ToBool, err: ParrotError) -> Result<(), ParrotError> {
+pub trait Unpackable<T> {
+    fn unpack(self) -> T;
+}
+
+impl Unpackable<bool> for bool {
+    fn unpack(self) -> bool {
+        self
+    }
+}
+
+impl<T> Unpackable<T> for Option<T> {
+    fn unpack(self) -> T {
+        self.unwrap()
+    }
+}
+
+impl<T, E> Unpackable<T> for Result<T, E>
+where
+    E: Debug,
+{
+    fn unpack(self) -> T {
+        self.unwrap()
+    }
+}
+
+pub fn verify<K, T: ToBool + Unpackable<K>>(
+    condition: T,
+    err: ParrotError,
+) -> Result<K, ParrotError> {
     if condition.to_bool() {
-        Ok(())
+        Ok(condition.unpack())
     } else {
         Err(err)
     }
---
+++ src/test/errors.rs
@@ -0,0 +1,34 @@
+use crate::errors::{verify, ParrotError};
+
+#[test]
+fn test_verify_bools() {
+    let x = true;
+    let x = verify(x, ParrotError::Other("not true"));
+    assert_eq!(x, Ok(true));
+
+    let x = false;
+    let x = verify(x, ParrotError::Other("not true"));
+    assert_eq!(x, Err(ParrotError::Other("not true")));
+}
+
+#[test]
+fn test_verify_options() {
+    let x = Some("🦜");
+    let x = verify(x, ParrotError::Other("not true"));
+    assert_eq!(x, Ok("🦜"));
+
+    let x: Option<&str> = None;
+    let x = verify(x, ParrotError::Other("not true"));
+    assert_eq!(x, Err(ParrotError::Other("not true")));
+}
+
+#[test]
+fn test_verify_results() {
+    let x: Result<&str, &str> = Ok("🦜");
+    let x = verify(x, ParrotError::Other("not true"));
+    assert_eq!(x, Ok("🦜"));
+
+    let x: Result<&str, &str> = Err("fatality");
+    let x = verify(x, ParrotError::Other("not true"));
+    assert_eq!(x, Err(ParrotError::Other("not true")));
+}
