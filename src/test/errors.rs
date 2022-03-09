a458da714c97484e5c7c9bc795e977456fddfa30
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-03-09 18:53:10 +0000

    test: better test values

--- src/test/errors.rs
+++ src/test/errors.rs
@@ -14,21 +14,21 @@ fn test_verify_bools() {
 #[test]
 fn test_verify_options() {
     let x = Some("🦜");
-    let x = verify(x, ParrotError::Other("not true"));
+    let x = verify(x, ParrotError::Other("not something"));
     assert_eq!(x, Ok("🦜"));
 
     let x: Option<&str> = None;
-    let x = verify(x, ParrotError::Other("not true"));
-    assert_eq!(x, Err(ParrotError::Other("not true")));
+    let x = verify(x, ParrotError::Other("not something"));
+    assert_eq!(x, Err(ParrotError::Other("not something")));
 }
 
 #[test]
 fn test_verify_results() {
     let x: Result<&str, &str> = Ok("🦜");
-    let x = verify(x, ParrotError::Other("not true"));
+    let x = verify(x, ParrotError::Other("not ok"));
     assert_eq!(x, Ok("🦜"));
 
     let x: Result<&str, &str> = Err("fatality");
-    let x = verify(x, ParrotError::Other("not true"));
-    assert_eq!(x, Err(ParrotError::Other("not true")));
+    let x = verify(x, ParrotError::Other("not ok"));
+    assert_eq!(x, Err(ParrotError::Other("not ok")));
 }
