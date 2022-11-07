437e8b284811de35284b2b1d906982a1802f97a4
Miguel Mano <aquelemiguel@gmail.com>
2022-11-07 21:56:47 +0000

    ci: apply clippy suggestions for rust 1.65.0 (#214)
    * ci: apply clippy suggestions for rust 1.65.0
    
    * refactor: switch to let-else statements
    
    * ci: move from rust 1.59 to 1.65
    
    * refactor: revert connection.rs

--- src/handlers/idle.rs
+++ src/handlers/idle.rs
@@ -21,31 +21,34 @@ pub struct IdleHandler {
 #[async_trait]
 impl EventHandler for IdleHandler {
     async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
-        if let EventContext::Track(track_list) = ctx {
-            // looks like the track list isn't ordered here, so the first track in the list isn't
-            // guaranteed to be the first track in the actual queue, so search the entire list
-            let bot_is_playing = track_list
-                .iter()
-                .any(|track| matches!(track.0.playing, PlayMode::Play));
+        let EventContext::Track(track_list) = ctx else {
+            return None;
+        };
 
-            // if there's a track playing, then reset the counter
-            if bot_is_playing {
-                self.count.store(0, Ordering::Relaxed);
-                return None;
-            }
+        // looks like the track list isn't ordered here, so the first track in the list isn't
+        // guaranteed to be the first track in the actual queue, so search the entire list
+        let bot_is_playing = track_list
+            .iter()
+            .any(|track| matches!(track.0.playing, PlayMode::Play));
 
-            if self.count.fetch_add(1, Ordering::Relaxed) >= self.limit {
-                let guild_id = self.interaction.guild_id?;
+        // if there's a track playing, then reset the counter
+        if bot_is_playing {
+            self.count.store(0, Ordering::Relaxed);
+            return None;
+        }
 
-                if self.manager.remove(guild_id).await.is_ok() {
-                    self.interaction
-                        .channel_id
-                        .say(&self.http, IDLE_ALERT)
-                        .await
-                        .unwrap();
-                }
+        if self.count.fetch_add(1, Ordering::Relaxed) >= self.limit {
+            let guild_id = self.interaction.guild_id?;
+
+            if self.manager.remove(guild_id).await.is_ok() {
+                self.interaction
+                    .channel_id
+                    .say(&self.http, IDLE_ALERT)
+                    .await
+                    .unwrap();
             }
         }
+
         None
     }
 }
