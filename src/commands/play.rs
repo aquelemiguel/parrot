6f7161755421de7e82a87c78c5e95d22e9a02808
Cycle Five <cycle.five@proton.me>
2023-09-04 12:36:46 -0400

    cargo fmt --all

--- src/commands/play.rs
+++ src/commands/play.rs
@@ -217,7 +217,9 @@ pub async fn play(
                     .ok_or(ParrotError::Other("failed to fetch playlist"))?;
 
                 for url in urls.iter() {
-                    let Ok(queue) = enqueue_track(&call, &QueryType::VideoLink(url.to_string())).await else {
+                    let Ok(queue) =
+                        enqueue_track(&call, &QueryType::VideoLink(url.to_string())).await
+                    else {
                         continue;
                     };
                     update_queue_messages(&ctx.http, &ctx.data, &queue, guild_id).await;
@@ -249,7 +251,8 @@ pub async fn play(
                     .ok_or(ParrotError::Other("failed to fetch playlist"))?;
 
                 for (idx, url) in urls.into_iter().enumerate() {
-                    let Ok(queue) = insert_track(&call, &QueryType::VideoLink(url), idx + 1).await else {
+                    let Ok(queue) = insert_track(&call, &QueryType::VideoLink(url), idx + 1).await
+                    else {
                         continue;
                     };
                     update_queue_messages(&ctx.http, &ctx.data, &queue, guild_id).await;
@@ -288,7 +291,9 @@ pub async fn play(
                 let mut insert_idx = 1;
 
                 for (i, url) in urls.into_iter().enumerate() {
-                    let Ok(mut queue) = insert_track(&call, &QueryType::VideoLink(url), insert_idx).await else {
+                    let Ok(mut queue) =
+                        insert_track(&call, &QueryType::VideoLink(url), insert_idx).await
+                    else {
                         continue;
                     };
 
